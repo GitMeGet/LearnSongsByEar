d310b69e112323e5d82195868540d8b19d07c793
GitMeGet <tanyupeng1994@gmail.com>
2019-05-16 16:54:29 +0800

    stub music player

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  state = {
+    selectedTrack: null
+  };
+
+  render() {
+    const list = [{ id: 1, title: "Campfire Story" }, {id: 2, title: "Booting Up"}].map(item => {
+      return (
+        <li
+          key={item.id}
+          onClick={() => this.setState({ selectedTrack: item.title })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {item.title}
+        </li>
+      );
+    });
+
+    return (
+      <>
+        <h1>My Little Player</h1>
+        <ul>{list}</ul>
+        <audio />
+      </>
+    );
+  }
 }
 
 export default App;
